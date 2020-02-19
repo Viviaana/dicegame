7813d10ca29c9937d297037a53b32ca42e196498
Katie Ingham <kjingham91@gmail.com>
2020-02-19 14:42:47 +0000

    changed dice image

--- script.js
+++ script.js
@@ -7,7 +7,28 @@ let sum = 0;
 //generating number when button is clicked and adding to total
 button.addEventListener("click", () =>{
     let number = Math.floor(Math.random() * 6) +1;
-    image.setAttribute('src',`./img/dice${number}.png`);
+    image.setAttribute('src',`./img/dice-${number}.png`);
     sum += number;
-    total.innerText = `Total score: ${sum}`;
+    if (number == 1){
+        total.innerText = "You lose!";
+        newgame.style.display = ""
+        button.style.display = "none"
+    }
+    else if (sum <20){
+        total.innerText = `Total score: ${sum}`;
+    }
+    else{
+        total.innerText = "Winner!!"
+        newgame.style.display = ""
+        button.style.display = "none"
+    }
+})
+
+newgame.addEventListener("click", () =>{
+    sum = 0;
+    total.innerText = "Total";
+    newgame.style.display = "none";
+    button.style.display = "";
+    image.setAttribute('src',`./img/dice-1.png`);
+
 })
